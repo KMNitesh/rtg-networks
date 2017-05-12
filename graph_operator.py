70b87380722f5eedc6d51438005dbfcfcb6d5c98
wacero666 <whezhao@ucdavis.edu>
2017-05-12 13:55:28 -0700

    hierarchical cluster worked within code group

--- graph_operator.py
+++ graph_operator.py
@@ -81,7 +81,7 @@ class GraphOperator:
             plt.plot(vec1[[j,k]],vec2[[j,k]])
         
                
-    def partition(self, standardVal = 0):
+    def _split_once(self, standardVal):
         secondEigVec = self.eigvecs[:,1]
         nodes = self._graph.nodes()
         A = []
@@ -91,7 +91,26 @@ class GraphOperator:
                 A.append(nodes[i])
             else:
                 B.append(nodes[i])
-        return (A,B)
+        return A, B
+    
+    def partition(self, n_groups = 2, standardVal = 0):
+        """
+        Return a list of groups, each group is a subgraph corresponding to one cluster
+        """
+   
+        sgs = [self]
+   
+        for i in range(n_groups - 1):
+            sgs.sort(key = lambda x : x._graph.number_of_nodes(), reverse = True)
+            largestgraph = sgs.pop(0)
+            A, B = largestgraph._split_once(standardVal)
+            g_A = largestgraph._graph.subgraph(A)
+            G_A = GraphOperator(g_A, self.matrix_type)
+            g_B = largestgraph._graph.subgraph(B)
+            G_B = GraphOperator(g_B, self.matrix_type)
+            sgs.extend([G_A, G_B])
+            
+        return [x._graph.nodes() for x in sgs]
     
     def removeEdge(self, A, B):
         edgelist = self._graph.edges()
@@ -116,13 +135,14 @@ class GraphOperator:
         if (labels):
             nx.draw_networkx_labels(self._graph, self.pos, self.labels)
        
-    def draw_partitionGraph(self, A, B, colorA = 'gold', colorB = 'palegreen', labels = True, node_size = 30, alpha = 0.5, width = 1):
+    def draw_partitionGraph(self, list_nodes, list_color, labels = True, node_size = 30, alpha = 0.5, width = 1):
         #if (self.labels == {}):
         #    self.getLabelsAndPos()
         
-        nx.draw_networkx_nodes(self._graph, self.pos, nodelist = A, node_color=colorA, node_size = node_size, alpha = alpha)
-        nx.draw_networkx_nodes(self._graph, self.pos, nodelist = B, node_color=colorB, node_size = node_size, alpha = alpha)
-        nx.draw_networkx_edges(self._graph, self.pos, edgelist = self.removeEdge(A, B), width = width)
+        for i in range(len(list_nodes)):
+            nx.draw_networkx_nodes(self._graph, self.pos, nodelist = list_nodes[i], node_color=list_color[i], node_size = node_size, alpha = alpha)
+        #nx.draw_networkx_nodes(self._graph, self.pos, nodelist = B, node_color=colorB, node_size = node_size, alpha = alpha)
+        nx.draw_networkx_edges(self._graph, self.pos, edgelist = self._graph.edges(), width = width)
         if (labels):
             nx.draw_networkx_labels(self._graph, self.pos, self.labels)
 
